ed0b09cb4e60d389d198398e312d95b0bcd5050e
OnFire29 <101292188+OnFire29@users.noreply.github.com>
2023-01-30 18:46:48 +0100

    aggiunto js

---
+++ script.js
@@ -0,0 +1,15 @@
+function move_titolo_notizia(){
+    onload.getElementById("").innerHTML="CIAO SONO UN TITOLO"
+}
+function move_descrizione_notizia(){
+    onload.getElementById("").innerHTML="CIAO SONO UNA DESCRIZIONE"
+}
+function move_data_notizia(){
+    onload.getElementById("").innerHTML="30/01/2023"
+}
+function move_img_notizia(){
+    onload.getElementById("").style.src="IMG"
+}
+function chiudi_popup_home(){
+    document.getElementsByClassName("").style.display="none";
+}
