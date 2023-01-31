7934c409425ccbef8df3b9c814d3f8739884341c
OnFire29 <101292188+OnFire29@users.noreply.github.com>
2023-01-31 21:57:58 +0100

    fixed link, bisogna aggiungere attività ai footer

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function move_titolo_notizia(){
-    onload.getElementById("").innerHTML="CIAO SONO UN TITOLO";
-}
-function move_descrizione_notizia(){
-    onload.getElementById("").innerHTML="CIAO SONO UNA DESCRIZIONE";
-}
-function move_data_notizia(){
-    onload.getElementById("").innerHTML="30/01/2023";
-}
-function move_img_notizia(){
-    onload.getElementById("").style.src="IMG";
-}
 function chiudi_popup(){
     const a = document.getElementById("popup_new_home");
     a.style.display="none";
